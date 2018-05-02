1e36a9d97419c966189a4428fd301adba0655025
AYOUB <ayoub.elkadhi@esprit.tn>
2018-05-02 13:28:55 +0200

    ajout des pages profile et reclamation

--- paw_mobile/src/com/mycompany/myapp/MenuForm.java
+++ paw_mobile/src/com/mycompany/myapp/MenuForm.java
@@ -9,6 +9,8 @@ import com.codename1.ui.Form;
 import com.codename1.ui.util.Resources;
 import forms.boutique.BoutiqueForm;
 import forms.boutique.PanierForm;
+import forms.reclamation.ReclamationForm;
+import forms.utilisateur.ProfileForm;
 
 /**
  *
@@ -26,19 +28,32 @@ public class MenuForm {
     public void affiche()
     {
         menu = new Form ("menu");
-              menu.getToolbar().addCommandToOverflowMenu("Panier", null, (e)
+        menu.getToolbar().addCommandToOverflowMenu("Panier", null, (e)
                 -> {
                 PanierForm bf = new PanierForm(theme);
-        bf.affichePanier();
-        }
+                bf.affichePanier();
+                }
         );
-         menu.getToolbar().addCommandToOverflowMenu("Boutique", null, (e)
+        menu.getToolbar().addCommandToOverflowMenu("Boutique", null, (e)
                 -> {
                 BoutiqueForm bf = new BoutiqueForm();
-        bf.afficherBoutique("Vetements");
-        }
+                bf.afficherBoutique("Vetements");
+                }
         );
-         
+        menu.getToolbar().addCommandToOverflowMenu("Profile", null, (e)
+                -> {
+                ProfileForm pf = new ProfileForm();
+                pf.afficher();
+                }
+        );
+        menu.getToolbar().addCommandToOverflowMenu("Réclamation", null, (e)
+                -> {
+                ReclamationForm rf = new ReclamationForm();
+                rf.afficher("Vetements");
+                }
+        );
+
+        
        menu.show();
     }
     
---
+++ paw_mobile/src/forms/reclamation/ReclamationForm.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forms.reclamation;
+
+import com.codename1.ui.Form;
+import com.codename1.ui.util.Resources;
+import com.codename1.ui.util.UIBuilder;
+
+/**
+ *
+ * @author AYOUB
+ */
+public class ReclamationForm {
+    Form currentform;
+    Resources theme;
+    private UIBuilder uib;
+    public void afficher(String vetements) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
